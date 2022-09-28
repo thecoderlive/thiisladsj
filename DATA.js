daa38fb61f07216d4cc741bd96703c84d7afe480
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 12:06:36 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "75236c8f-025f-43ad-8210-cc55f850d5da",
+    "home_feed": [
+        {
+            "id": "9bc060d5-fd7f-4d20-bc83-cbd2969c82c5",
+            "artist_name": "eight",
+            "home_carousel": [
+                {
+                    "id": "1683b2a0-cb72-4687-a1ff-bc1e45701815",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png"
+                },
+                {
+                    "id": "6080ad94-12bc-4006-bb58-bbda1a95b697",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "de49d129-a5b1-4fa3-b1b1-71d6d0e9940a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "40255a42-d40c-4ed1-8dd5-a6a68cb011ec",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+                },
+                {
+                    "id": "eaa5a81c-774a-4894-ab98-d5e4772ac64f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                }
+            ],
+            "art_caption": "manager",
+            "art_description": "Data magazine time various. Window real their should sometimes take."
+        },
+        {
+            "id": "025d528e-cbd1-45a7-b1dc-597ce4133e2e",
+            "artist_name": "human",
+            "home_carousel": [
+                {
+                    "id": "9a1cb575-774f-4074-9467-c8e839b2a140",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "f48b6ad2-b847-45c6-b4ba-227d20d7abc1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                },
+                {
+                    "id": "5065aef2-6180-4893-a2e2-39c754e5dba7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/124.png"
+                },
+                {
+                    "id": "963e1c98-1ecd-4d60-a7d5-45a5b08efa44",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "1bcf6964-3425-4a0c-99d5-24f105bc3ad4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                }
+            ],
+            "art_caption": "piece",
+            "art_description": "Soon heart up also discover stop rise. Drive agent even arm view instead student."
+        },
+        {
+            "id": "59106810-99ef-4cc1-afba-627a9a3ef505",
+            "artist_name": "fill",
+            "home_carousel": [
+                {
+                    "id": "d826e2cf-445c-4f52-a110-d0c9c61c074a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "a12c6d73-bc41-4e65-b3b5-631f4aa526a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                },
+                {
+                    "id": "56a7ce19-af97-4fbc-84f2-d00edad749ba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                },
+                {
+                    "id": "728ad986-7882-4d38-a8d9-70a3598102c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "b5b0a749-a73c-4735-98ba-09373f7795af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                }
+            ],
+            "art_caption": "speak",
+            "art_description": "Year material account pay old environmental defense."
+        },
+        {
+            "id": "eec8fb1e-e766-4f47-b97b-79bdf0897ec8",
+            "artist_name": "far",
+            "home_carousel": [
+                {
+                    "id": "9eb683c4-e540-4cd9-a96f-93fc8db7ea87",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+                },
+                {
+                    "id": "86f4a4c4-101d-405a-a349-fb3cec1e5a4f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "66279d5c-8eac-42ed-befa-89b79314c3f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/156.png"
+                },
+                {
+                    "id": "f4041506-6f8a-4424-b498-5d2e7c7d04fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "639c210a-3862-4cda-9102-2ef5b80bc7af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                }
+            ],
+            "art_caption": "let",
+            "art_description": "Unit week late she pick. Against section college admit. Really make order participant hard do spend reveal."
+        },
+        {
+            "id": "41e12b70-e768-44fb-aeba-46f58d9b168d",
+            "artist_name": "foreign",
+            "home_carousel": [
+                {
+                    "id": "3c30cc67-8a2a-44ab-aec0-b010b40aa01b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "a2dfec57-602b-4ff8-91e5-88d4dfb56333",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "01a8c8e6-835c-4668-b8da-928a325ce35b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "f6664529-cb17-4e58-9df2-9d605fc368d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "e4c13fc1-0359-4a35-ba69-f2fa2a121512",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                }
+            ],
+            "art_caption": "different",
+            "art_description": "Pick building day statement. Bit sound shoulder on. Avoid hot study stuff treatment create."
+        }
+    ]
+}
